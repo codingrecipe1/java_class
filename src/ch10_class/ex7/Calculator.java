0882deb61b46e1aee75b655e6b532f2e3b3e55dc
codingrecipe1 <codingrecipe1@gmail.com>
2023-12-12 14:39:17 +0900

    객체지향-method 연습(계산기)

--- src/ch10_class/ex7/Calculator.java
+++ src/ch10_class/ex7/Calculator.java
@@ -1,6 +1,9 @@
 package ch10_class.ex7;
 
+import java.util.Scanner;
+
 public class Calculator {
+    Scanner scanner = new Scanner(System.in);
     /**
      * 덧셈 메서드
      * method name: sum
@@ -9,6 +12,14 @@ public class Calculator {
      * 실행내용
      *  - 매개변수로 전달 받은 2개 값의 합을 출력
      */
+    /**
+     *
+     * @param num1
+     * @param num2
+     */
+    public void sum(int num1, int num2) {
+        System.out.println("합계: " + (num1 + num2));
+    }
 
     /**
      * 뺄셈 메서드
@@ -18,6 +29,10 @@ public class Calculator {
      * 실행내용
      *  - 매개변수로 전달 받은 2개 값의 차를 출력
      */
+    public void sub(int num1, int num2) {
+        int result = num1 - num2;
+        System.out.println("result = " + result);
+    }
 
     /**
      * 곱셈 메서드
@@ -27,6 +42,16 @@ public class Calculator {
      * 실행내용
      *  - 매개변수로 전달 받은 2개 값의 곱 결과를 리턴
      */
+    /**
+     *
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public int mul(int num1, int num2) {
+        int result = num1 * num2;
+        return result;
+    }
 
     /**
      * 나눗셈 메서드
@@ -37,4 +62,12 @@ public class Calculator {
      *  - 스캐너로 나눗셈에 필요한 정수 2개를 입력받음
      *  - 나눗셈 결과를 리턴
      */
+    public int div() {
+        System.out.print("첫번째 숫자: ");
+        int num1 = scanner.nextInt();
+        System.out.print("두번째 숫자: ");
+        int num2 = scanner.nextInt();
+        int result = num1 / num2;
+        return result;
+    }
 }
