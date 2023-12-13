3e8ecd91ef071440a996514178ef6d9665c97670
codingrecipe1 <codingrecipe1@gmail.com>
2023-12-13 09:37:26 +0900

    클래스연습-회원

---
+++ src/ch10_class/ex10/Member.java
@@ -0,0 +1,20 @@
+package ch10_class.ex10;
+
+/**
+ * 회원 정보 클래스
+ * 회원 정보
+ *    - 이메일(memberEmail)
+ *    - 비밀번호(memberPassword)
+ *    - 이름(memberName)
+ *    - 전화번호(memberMobile)
+ *  getter/setter
+ *  생성자 2가지
+ *  로그인 처리 메서드
+ *    name: memberLogin
+ *    parameter: memberEmail, memberPassword
+ *    return type: boolean
+ *    실행내용: 이메일, 비밀번호가 일치하면 true,
+ *              일치하지 않으면 false 리턴
+ */
+public class Member {
+}
