57e555ce186a820fc30e5aa2ee4a08da2f18fe00
codingrecipe1 <codingrecipe1@gmail.com>
2023-12-14 13:52:56 +0900

    ArrayList - Book 객체

--- src/ch11_array/ex03/Book.java
+++ src/ch11_array/ex03/Book.java
@@ -16,5 +16,71 @@ package ch11_array.ex03;
  toString method
  */
 public class Book {
+    private Long id;
+    private String bookTitle;
+    private String bookAuthor;
+    private int bookPrice;
+    private String bookPublisher;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public int getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(int bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+
+    public String getBookPublisher() {
+        return bookPublisher;
+    }
+
+    public void setBookPublisher(String bookPublisher) {
+        this.bookPublisher = bookPublisher;
+    }
+
+    public Book() {
+    }
+
+    public Book(Long id, String bookTitle, String bookAuthor, int bookPrice, String bookPublisher) {
+        this.id = id;
+        this.bookTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+        this.bookPrice = bookPrice;
+        this.bookPublisher = bookPublisher;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", bookTitle='" + bookTitle + '\'' +
+                ", bookAuthor='" + bookAuthor + '\'' +
+                ", bookPrice=" + bookPrice +
+                ", bookPublisher='" + bookPublisher + '\'' +
+                '}';
+    }
 }
