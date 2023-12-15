d8723b1fa6a3674a9d9a0837c011fdef1c4e152c
codingrecipe1 <codingrecipe1@gmail.com>
2023-12-15 10:10:40 +0900

    클래스 분리 예제 - 등록기능

--- src/ch11_classes/ex01/StudentService.java
+++ src/ch11_classes/ex01/StudentService.java
@@ -19,8 +19,21 @@ public class StudentService {
         studentRepository.method2(str1);
     }
 
+    /**
+     * StudentDTO 객체를 생성하고
+     * Repository의 method3로 DTO 객체를 전달(호출)함.
+     */
     public void method3() {
         System.out.println("StudentService.method3");
+        StudentDTO studentDTO = new StudentDTO("학생1", "20231111", "컴퓨터공학", "010-1111-1111");
+//        StudentDTO studentDTO = new StudentDTO();
+        boolean result = studentRepository.method3(studentDTO);
+        System.out.println("result = " + result);
+        if (result) {
+            System.out.println("학생등록 성공");
+        } else {
+            System.out.println("학생등록 실패");
+        }
     }
 
 
