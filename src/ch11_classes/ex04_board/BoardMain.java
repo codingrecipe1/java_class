0a29688bbd860d958817a4277c674925cf5f2896
codingrecipe1 <codingrecipe1@gmail.com>
2023-12-19 09:53:53 +0900

    게시판 프로젝트 - 프로젝트 세팅

--- src/ch11_classes/ex04_board/BoardMain.java
+++ src/ch11_classes/ex04_board/BoardMain.java
@@ -7,32 +7,26 @@ public class BoardMain {
         Scanner scan = new Scanner(System.in);
         boolean run = true;
         int selectNo = 0;
-        MemberService memberService = new MemberService();
+        BoardService boardService = new BoardService();
 
         while (run) {
             System.out.println("-----------------------------------------------------------------------------------------");
-            System.out.println("1.회원가입 | 2.로그인 | 3.목록조회 | 4.정보수정 | 5.탈퇴 | 6.로그아웃 | 0.종료");
+            System.out.println("1.글작성 | 2.글목록 | 3.글조회 | 4.글수정 | 5.글삭제 | 6.검색 | 0.종료");
             System.out.println("-----------------------------------------------------------------------------------------");
             System.out.print("선택> ");
             selectNo = scan.nextInt();
             if (selectNo == 1) {
-                // 호출하는 문장 작성
-                memberService.save();
+
             } else if (selectNo == 2) {
-                // 호출하는 문장 작성
-                memberService.login();
+
             } else if (selectNo == 3) {
-                // 호출하는 문장 작성
-                memberService.findAll();
+
             } else if (selectNo == 4) {
-                // 호출하는 문장 작성
-                memberService.update();
+
             } else if (selectNo == 5) {
-                // 호출하는 문장 작성
-                memberService.delete();
+
             } else if (selectNo == 6) {
-                // 호출하는 문장 작성
-                memberService.logout();
+
             } else if (selectNo == 0) {
                 run = false;
             }
