2bcf4b80fe38cf29ab8e2f94b36f0aa058918551
codingrecipe1 <codingrecipe1@gmail.com>
2023-12-26 16:09:41 +0900

    상속

--- src/ch13_map/ex03_member/MemberService.java
+++ src/ch13_map/ex03_member/MemberService.java
@@ -1,6 +1,7 @@
 package ch13_map.ex03_member;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class MemberService {
@@ -55,9 +56,9 @@ public class MemberService {
     }
 
     public void findAll() {
-        List<MemberDTO> memberDTOList = memberRepository.findAll();
-        for (MemberDTO memberDTO: memberDTOList) {
-            System.out.println("memberDTO = " + memberDTO);
+        Map<Long, MemberDTO> memberDTOMap = memberRepository.findAll();
+        for (Long i : memberDTOMap.keySet()) {
+            System.out.println(memberDTOMap.get(i));
         }
     }
 
